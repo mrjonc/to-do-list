2ba027e1a94cf839620475b25fc7347f1a4a3701
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-04-14 19:00:53 -0300

    Adicionando o modal ao clicar em excluir tarefa

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -43,7 +43,7 @@ function showTasks() {
           <button onclick="openEditModal(${index})" title="Editar">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/></svg>
           </button>
-          <button onclick="deleteTask(${index})" title="Excluir">
+          <button onclick="openDeleteModal(${index})" title="Excluir">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/></svg>
           </button>
         </div>
@@ -51,13 +51,30 @@ function showTasks() {
   });
 }
 
-function deleteTask(index) {
-  if (confirm("Deseja apagar essa tarefa?")) {
-    let values = JSON.parse(localStorage.getItem(localStorageName)) || [];
-    values.splice(index, 1);
-    localStorage.setItem(localStorageName, JSON.stringify(values));
-    showTasks();
-  }
+function openDeleteModal(index) {
+  document.getElementById("deleteTaskIndex");
+
+  const modalDelete = new bootstrap.Modal(
+    document.getElementById("modalDelete"),
+  );
+
+  modalDelete.show();
+}
+
+function saveDelete() {
+  let values = JSON.parse(localStorage.getItem(localStorageName)) || [];
+  let index = document.getElementById("deleteTaskIndex").value;
+
+  values.splice(index, 1);
+
+  localStorage.setItem(localStorageName, JSON.stringify(values));
+
+  const modalElement = document.getElementById("modalDelete");
+  const modalInstance = bootstrap.Modal.getInstance(modalElement);
+
+  modalInstance.hide();
+
+  showTasks();
 }
 
 // --- FUNÇÕES DO MODAL ---
